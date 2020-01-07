edfafc370f2e320e225e00e792a319627322a6c6
dieisonnonnenmacher <dieisonnonnenmacher@gmail.com>
2020-01-06 23:23:05 -0300

    ajuste api-banco projeto spring

---
+++ api-banco/src/test/java/br/com/ferrosul/apibanco/ApiBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferrosul.apibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
